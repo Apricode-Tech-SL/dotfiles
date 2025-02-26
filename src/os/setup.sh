92f7517d2369f324e72615e26dc04cfc837ef8f7
Dídac Rios <didac.rios@apricodetech.com>
2025-02-26 15:19:16 +0100

    Update setup.sh
    WIP, just added some pseudocode and todo instructions

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -6,28 +6,47 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # Ensure Homebrew is installed
 if ! command -v brew &>/dev/null; then
-    print_in_yellow("Installing Homebrew...")
+    print_in_purple " • Installing Homebrew...\n\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-print_in_yellow("Updating Homebrew...")
+print_in_purple "• Updating Homebrew...\n\n"
 brew update
 
 # Install packages
 BREW_PACKAGES=(
+    coreutils
     docker
     docker-completion
+    fnm
+    htop
+    jq
+    openssh
+    yq
+    zsh
 )
 
 CASK_PACKAGES=(
+    # IDEs
     visual-studio-code
     jetbrains-toolbox
     intellij-idea-ce
+    dbeaver-community
+
+    # window management
     rectangle
     raycast
-    google-chrome
+
+    # browsers
     firefox
+    google-chrome
+
+    # tools
     obsidian
+    onyx
+
+    # terminals
+    warp
 )
 
 echo "Installing brew packages..."
@@ -40,17 +59,12 @@ brew install --cask "${CASK_PACKAGES[@]}"
 echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-# Install fnm (Fast Node Manager)
-echo "Installing fnm..."
-brew install fnm
-
 # Configure p10k
 echo "Configuring powerlevel10k..."
 p10k configure
 
 # Copy .gitconfig and gitignore to home directory
-echo "Copying .gitconfig and .gitignore to home directory..."
+print_in_purple "• Copying .gitconfig and .gitignore to home directory...\n\n"
 cp -iV .gitconfig $HOME
 cp -iV .gitignore $HOME
 
@@ -61,20 +75,19 @@ grep -qxF "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions
 
 source "$ZSHRC"
 
-# ask fot global git username and replace CHANGEME_GIT_NAME and CHANGEME_GIT_EMAIL with input in copied .gitconfig
-echo "Enter your global git username:"
+# ask fot global git username and email
+print_question "Enter your global git username:"
 read GIT_USERNAME
-sed -i '' "s/CHANGEME_GIT_NAME/$GIT_USERNAME/g" $HOME/.gitconfig
 
-echo "Enter your global git email:"
+print_question("Enter your global git email:")
 read GIT_EMAIL
-sed -i '' "s/CHANGEME_GIT_EMAIL/$GIT_EMAIL/g" $HOME/.gitconfig
 
-print_in_yellow ".gitconfig" changed
-
-print_in_green "macOS setup completed!"
+# Create .gitconfig.local with the [user] information 
+./create_local_config_files.sh GIT_USERNAME GIT_EMAIL
 
 
+print_success ".gitconfig.local created"
+
+print_success "macOS setup completed!"
 
 
-echo "macOS setup completed!"
