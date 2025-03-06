eecdb843a4a5a405709504aebee220a04614163e
Dídac Rios <didac.rios@gmail.com>
2025-03-06 17:28:54 +0100

    Try manually install of the zsh

--- src/os/apps/install.sh
+++ src/os/apps/install.sh
@@ -52,8 +52,14 @@ brew install --cask "${CASK_PACKAGES[@]}"
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
+# Clone the Oh My Zsh repository directly instead of using the installer script
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 source "$ZSHRC"
+
+if [ "$SHELL" != "$(which zsh)" ]; then
+  chsh -s "$(which zsh)"
+  print_success "Set Zsh as the default shell"
+fi
