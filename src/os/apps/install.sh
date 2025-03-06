926e5dbbd8202831836e88f354cfd18ee5156903
Dídac Rios <didac.rios@gmail.com>
2025-03-06 17:12:26 +0100

    try again the unattended option

--- src/os/apps/install.sh
+++ src/os/apps/install.sh
@@ -52,8 +52,7 @@ brew install --cask "${CASK_PACKAGES[@]}"
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
-export RUNZSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
