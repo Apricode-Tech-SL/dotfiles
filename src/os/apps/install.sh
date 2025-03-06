7175cdae6e803f46fb37aa1a1e9ca09051c6f440
Dídac Rios <didac.rios@gmail.com>
2025-03-06 16:41:12 +0100

    check for subdirectories created of oh my zsh to ensure is installed

--- src/os/apps/install.sh
+++ src/os/apps/install.sh
@@ -57,7 +57,7 @@ RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Ensure Oh My Zsh is fully installed before proceeding
-OMZ_DIR="$HOME/.oh-my-zsh"
+OMZ_DIR="$HOME/.oh-my-zsh/plugins"
 while [ ! -d "$OMZ_DIR" ]; do
     echo "Waiting for Oh My Zsh installation to complete..."
     sleep 5
